eaa176f9837b0ae3fa062138e7b88eed84b46156
toriv02 <torivmalahov@gmail.com>
2024-04-14 14:46:40 +0800

    first commit

---
+++ task1lab1Tprog/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+public class Logic
+{
+    //логика программы в которой происходит поиск ответа на поставленную задачу с текущими данными
+    public static Double solution(double x)
+    { 
+        //проверка принадлежности числда х 1 из диапазонов
+        if (x <= 0) return 0; 
+        else if (0 < x && x <= 1) return x;
+        else return x * x;
+    }
+}
+    class Program
+{
+    static void Main()
+    {
+        try //обработка ошибки, при неверно введённом формате данных
+        {
+            //основная часть программы в которой происходит ввод данных и вывод ответа.
+            Console.WriteLine("Введите число:");
+            Double x = Double.Parse(Console.ReadLine());
+            Console.WriteLine("Ответ:");
+            var output = Logic.solution(x);
+            Console.WriteLine(output); 
+        }
+        catch (FormatException) { Console.WriteLine("формат введённых данных некорректен, попробуйте ещё раз"); }
+    }
+}
---
+++ task1lab1TprogTests/LogicTests.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    namespace FirstApp.Tests
+    {
+        [TestClass()]
+        public class LogicTests
+        {
+            [TestMethod()]//1-й тест: введенно число из отрицательного диапазона
+            public void getnegativenumber()
+            {
+                double x = -7;
+                Double anser = Logic.solution(x);
+                Assert.AreEqual(0, anser);
+            }
+
+            [TestMethod()]//2-й тест: проверка границы диапазона диапазона, а именно числа 0
+            public void getzero()
+            {
+                double x = 0;
+                Double anser = Logic.solution(x);
+                Assert.AreEqual(0, anser);
+            }
+            [TestMethod()]//3-й тест: проверка границы диапазона диапазона, а именно числа 1
+            public void getone()
+            {
+                double x = 1;
+                Double anser = Logic.solution(x);
+                Assert.AreEqual(1, anser);
+            }
+            [TestMethod()]//4-й тест:  введенно число из диапазона от 0 до 1
+            public void getfraction()
+            {
+                double x = 0.56;
+                Double anser = Logic.solution(x);
+                Assert.AreEqual(0.56, anser);
+            }
+            [TestMethod()]//5-й тест:  введенно число из положительного диапазона
+            public void getpositivenumber()
+            {
+                double x = 4;
+                Double anser = Logic.solution(x);
+                Assert.AreEqual(16, anser);
+            }
+        }
+    }
+
+}
